7e2b79e338148005f3166553db60320ed62dcd59
__sp__ <shyuep@gmail.com>
2014-04-05 22:44:20 -0700

    Correct some sg errors.

--- symmetry/groups.py
+++ symmetry/groups.py
@@ -55,24 +55,29 @@ class PointGroup(SymmetryGroup):
         """
         self.generators = [GENERATOR_MATRICES[c]
                            for c in POINT_GROUP_ENC[int_symbol]]
-        symm_ops = []
-        symm_ops.extend(self.generators)
-        new_ops = self.generators
-        while len(new_ops) > 0:
-            gen_ops = []
-            for g1, g2 in product(new_ops, symm_ops):
-                op = np.dot(g1, g2)
-                if not in_array_list(symm_ops, op):
-                    gen_ops.append(op)
-                    symm_ops.append(op)
-            new_ops = gen_ops
-        self.symmetry_ops = symm_ops
+        self.symmetry_ops = generate_full_symm(self.generators)
 
 
 class SpaceGroup(SymmetryGroup):
 
     def __init__(self, int_symbol):
-        pass
+        enc = list(SPACE_GROUP_ENC[int_symbol]["enc"])
+        inversion = int(enc.pop(0))
+        ngen = int(enc.pop(0))
+        symm_ops = []
+        if inversion:
+            symm_ops.append([[-1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0],
+                             [0, 0, 0, 1]])
+        for i in xrange(ngen):
+            m = np.eye(4)
+            m[:3, :3] = GENERATOR_MATRICES[enc.pop(0)]
+            m[0, 3] = TRANSLATIONS[enc.pop(0)]
+            m[1, 3] = TRANSLATIONS[enc.pop(0)]
+            m[2, 3] = TRANSLATIONS[enc.pop(0)]
+            symm_ops.append(m)
+
+        print symm_ops
+        self.symmetry_ops = generate_full_symm(symm_ops)
 
 
 def in_array_list(array_list, a):
@@ -87,14 +92,34 @@ def in_array_list(array_list, a):
     return np.any(np.all(np.equal(array_list, a[None, :]), axes))
 
 
-if __name__ == "__main__":
-    for k in POINT_GROUP_ENC.keys():
-        pg = PointGroup(k)
-        print "Order of point group %s is %d" % (k, len(pg.symmetry_ops))
+def generate_full_symm(ops):
+    symm_ops = list(ops)
+    new_ops = ops
+    while len(new_ops) > 0:
+        gen_ops = []
+        for g1, g2 in product(new_ops, symm_ops):
+            op = np.dot(g1, g2)
+            if not in_array_list(symm_ops, op):
+                gen_ops.append(op)
+                symm_ops.append(op)
+        new_ops = gen_ops
+        print symm_ops
+        break
+    return symm_ops
 
+
+if __name__ == "__main__":
     from sympy import symbols
-    x, y, z = symbols("x y z")
-    p = [x,y,z]
-    pg = PointGroup("m-3m")
-    for r in pg.get_orbit(p):
-        print r
+
+    # for k in POINT_GROUP_ENC.keys():
+    #     pg = PointGroup(k)
+    #     print "Order of point group %s is %d" % (k, len(pg.symmetry_ops))
+    #
+    # x, y, z = symbols("x y z")
+    # p = [x,y,z]
+    # pg = PointGroup("m-3m")
+    # for r in pg.get_orbit(p):
+    #     print r
+
+    sg = SpaceGroup("Fm-3m")
+    print len(sg.symmetry_ops)
