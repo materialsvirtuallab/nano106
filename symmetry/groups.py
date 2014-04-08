21012d7ed992974197320f90b56f754b8dae2476
__sp__ <shyuep@gmail.com>
2014-04-08 15:31:14 -0700

    Fix ref and names.

--- symmetry/groups.py
+++ symmetry/groups.py
@@ -7,10 +7,10 @@ TODO: Modify module doc.
 from __future__ import division
 
 __author__ = "Shyue Ping Ong"
-__copyright__ = "Copyright 2012, The Materials Project"
+__copyright__ = "Copyright 2013, The Materials Virtual Lab"
 __version__ = "0.1"
 __maintainer__ = "Shyue Ping Ong"
-__email__ = "shyuep@gmail.com"
+__email__ = "ongsp@ucsd.edu"
 __date__ = "4/4/14"
 
 import os
