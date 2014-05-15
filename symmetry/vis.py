76bfea4c12a0ba86a637fa486621f48d46528929
__sp__ <shyuep@gmail.com>
2014-05-14 21:34:16 -0700

    Fix PS3 Q5 solution.

--- symmetry/vis.py
+++ symmetry/vis.py
@@ -19,6 +19,7 @@ import itertools
 import numpy as np
 from pymatgen import Lattice
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 from pymatgen.util.plotting_utils import get_publication_quality_plot
 
 
@@ -42,34 +43,43 @@ class SpaceGroupVisualizer(object):
         kw = {k: params[k] for k in inspect.getargspec(func).args}
         lattice = func(**kw)
         global plt
+        fig = plt.figure(figsize=(10, 10))
+        #ax = fig.add_subplot(111, projection='3d')
         for i in range(2):
             plt.plot([0, lattice.matrix[i][0]], [0, lattice.matrix[i][1]],
                      'k-')
-
+        plt.plot([lattice.matrix[0][0], lattice.matrix[0][0]],
+                 [0, lattice.matrix[1][1]],
+                     'k-')
+        plt.plot([0, lattice.matrix[0][0]],
+                 [lattice.matrix[1][1], lattice.matrix[1][1]],
+                 'k-')
+        
         l = np.arange(0, 0.02, 0.02 / 100)
-        theta = np.arange(0, 2 * np.pi, 2 * np.pi / 100) - np.pi / 2
+        theta = np.arange(0, 4 * np.pi, 4 * np.pi / 100) - np.pi / 2
 
         x = l * np.cos(theta) + 0.025
-        y = l * np.sin(theta) + 0.031
-        d = np.array(zip(x, y, [0.01] * len(x), [1] * len(x)))
+        y = l * np.sin(theta) + 0.025
+        z = 0.001 * theta + 0.02
+        d = np.array(zip(x, y, z, [1] * len(x)))
 
         for op in sg.symmetry_ops:
             dd = np.dot(op, d.T).T
-            for tx, ty in itertools.product((0, 1), (0, 1)):
+            for tx, ty in itertools.product((-1, 0, 1), (-1, 0, 1)):
                 ddd = dd[:, 0:3] + np.array([tx, ty, 0])[None, :]
-                color = "r" if ddd[0, 2] > 0.5 else "b"
+                color = "r" if 0.5 < ddd[0, 2] > 1 else "b"
                 coords = lattice.get_cartesian_coords(ddd[:, 0:3])
                 plt.plot(coords[:, 0], coords[:, 1], color + "-")
 
-        plt.plot(x, y, 'k-')
+        #plt.plot(x, y, 'k-')
         max_l = max(params['a'], params['b'])
-        plt = get_publication_quality_plot(8, 8, plt)
+        #plt = get_publication_quality_plot(8, 8, plt)
+        #plt.savefig('test2png.png',dpi=100)
         lim = [-max_l * 0.1, max_l * 1.1]
         plt.xlim(lim)
         plt.ylim(lim)
         plt.tight_layout()
         plt.show()
-        print lattice.lengths_and_angles
 
 if __name__ == "__main__":
     from symmetry.groups import SpaceGroup
